cfbb6338a6a9a3b54ca4f9707408455129938f80
Adilet-312 <dyishaliev.adil@gmail.com>
2025-12-08 21:58:25 +0000

    created file input.sh so the user could input variables

--- Homework-14/input.sh
+++ Homework-14/input.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo "Hi, my name is $1, and I'm $2 years old. Every morning, $1 likes to spend time $3.
-As a $2-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $4.
-I code in $4 almost every day!
-One day, I hope my love for $4 will help me achieve my dream of $5."
-echo "When I'm not busy working toward $5, you'll find me $3 to relax."
-echo "That's a little aboute me, $1 - a $2-year-old who loves $4, enjoys $3, and dreams of $5!"
+echo "What is your name?"
+read name
+echo "What is your age?"
+read age
+echo "What is your favorite language?"
+read favorite_language
+echo "What is your hobby?"
+read hobby
+echo "What is your dream?"
+read dream
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little aboute me, $name - a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
