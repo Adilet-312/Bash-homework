1144684645ec869e912345024f5cfdacfc0cd2ee
Adilet-312 <dyishaliev.adil@gmail.com>
2025-12-08 22:21:19 +0000

    created file math.sh with 2 variables so the user could input values and do calculation

---
+++ Homework-14/math.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter 2 any numbers: " number1 number2
+
+echo $number1+$number2=$((number1+number2))
+echo $number1-$number2=$((number1-number2))
+echo $number1*$number2=$((number1*number2))
+echo $number1/$number2=$((number1/number2))
+
