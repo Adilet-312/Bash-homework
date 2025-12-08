b01cad5d202fed10c4adb989448f55c4a3330993
Adilet-312 <dyishaliev.adil@gmail.com>
2025-12-08 06:52:27 +0000

    added 5 variables: name, age, favorite_language, hobby, dream

--- Homework-14/introduce.sh
+++ Homework-14/introduce.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+name=Ali
+age=21
+favorite_language=java
+hobby=coding
+dream=automation
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream."
+echo "When I'm not busy working toward $dream, you'll find me $hobby to relax."
+echo "That's a little aboute me, $name - a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
